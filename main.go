a89820f4ae10d344d2d6e28ecbf231ac443e5d56
arricodyanto <arricohandyanto@gmail.com>
2023-11-08 01:35:26 +0700

    Add CRUD endpoints for master Employee

--- main.go
+++ main.go
@@ -17,6 +17,13 @@ func main() {
 	router.PUT("/customers/:id", transactions.UpdateCustomerById)
 	router.DELETE("/customers/:id", transactions.DeleteCustomerById)
 
+	// Employees
+	router.GET("/employees/", transactions.GetAllEmployees)
+	router.GET("/employees/:id", transactions.GetEmployeeById)
+	router.POST("/employees", transactions.CreateNewEmployee)
+	router.PUT("/employees/:id", transactions.UpdateEmployeeById)
+	router.DELETE("/employees/:id", transactions.DeleteEmployeeById)
+
 	// Products
 	router.GET("/products", transactions.GetAllProducts)
 	router.GET("/products/:id", transactions.GetProductById)
--- transactions/customer.go
+++ transactions/customer.go
@@ -1,6 +1,7 @@
 package transactions
 
 import (
+	"challenge-goapi/entity"
 	"net/http"
 	"strconv"
 
@@ -47,7 +48,7 @@ func GetCustomerById(c *gin.Context) {
 
 	customerId, err := strconv.Atoi(id)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Product ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Customer ID"})
 		return
 	}
 
@@ -98,7 +99,7 @@ func UpdateCustomerById(c *gin.Context) {
 
 	customerId, err := strconv.Atoi(id)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Product ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Customer ID"})
 		return
 	}
 
@@ -166,4 +167,161 @@ func DeleteCustomerById(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Successfully Deleted Customer '" + id + "'", "data": "OK"})
+}
+
+func GetAllEmployees(c *gin.Context) {
+	query := "SELECT id, name, phone_number, address, is_employee FROM mst_customer WHERE is_employee = true;"
+
+	rows, err := db.Query(query)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer rows.Close()
+
+	var matchedEmployee []entity.Customer
+	for rows.Next() {
+		var employee entity.Customer
+		err := rows.Scan(&employee.Id, &employee.Name, &employee.PhoneNumber, &employee.Address, &employee.IsEmployee)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		matchedEmployee = append(matchedEmployee, employee)
+	}
+
+	if len(matchedEmployee) > 0 {
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully Get Employees Data", "data": matchedEmployee})
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Employees Not Found!"})
+	}
+}
+
+func GetEmployeeById(c *gin.Context) {
+	id := c.Param("id")
+
+	employeeId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Employee ID"})
+		return
+	}
+
+	query := "SELECT id, name, phone_number, address, is_employee FROM mst_customer WHERE id = $1 AND is_employee = true;"
+
+	var matchedEmployee entity.Customer
+	err = db.QueryRow(query, employeeId).Scan(&matchedEmployee.Id, &matchedEmployee.Name, &matchedEmployee.PhoneNumber, &matchedEmployee.Address, &matchedEmployee.IsEmployee)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Employee Not Found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully Get Employee Data", "data": matchedEmployee})
+}
+
+func CreateNewEmployee(c *gin.Context) {
+	var newEmployee entity.Customer
+	err := c.ShouldBind(&newEmployee)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var maxId int
+	queryMaxId := "SELECT MAX(id) FROM mst_customer;"
+	err = db.QueryRow(queryMaxId).Scan(&maxId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed Getting New ID for New Employee"})
+		return
+	}
+
+	employeeId := maxId + 1
+	isEmployee := true
+
+	queryInsert := "INSERT INTO mst_customer (id, name, phone_number, address, is_employee) VALUES ($1, $2, $3, $4, $5);"
+	_, err = db.Exec(queryInsert, employeeId, newEmployee.Name, newEmployee.PhoneNumber, newEmployee.Address, isEmployee)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	newEmployee.Id = employeeId
+	newEmployee.IsEmployee = isEmployee
+	c.JSON(http.StatusCreated, gin.H{"message": "Successfully Created New Employee", "data": newEmployee})
+}
+
+func UpdateEmployeeById(c *gin.Context) {
+	id := c.Param("id")
+
+	employeeId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Employee ID"})
+		return
+	}
+
+	var isEmployee bool
+	queryCheckEmployee := "SELECT is_employee FROM mst_customer WHERE id = $1;"
+
+	err = db.QueryRow(queryCheckEmployee, employeeId).Scan(&isEmployee)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to Check Employee Status"})
+		return
+	}
+
+	if !isEmployee {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Failed to Updated This Customer, Forbidden!"})
+		return
+	}
+
+	var updatedEmployee entity.Customer
+	err = c.ShouldBind(&updatedEmployee)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	query := "UPDATE mst_customer SET name = $2, phone_number = $3, address = $4 WHERE id = $1;"
+
+	_, err = db.Exec(query, employeeId, updatedEmployee.Name, updatedEmployee.PhoneNumber, updatedEmployee.Address)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEmployee.Id = employeeId
+	updatedEmployee.IsEmployee = true
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully Updated Employee '" + id + "'", "data": updatedEmployee})
+}
+
+func DeleteEmployeeById(c *gin.Context) {
+	id := c.Param("id")
+
+	employeeId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Employee ID"})
+		return
+	}
+
+	var isEmployee bool
+	queryCheckEmployee := "SELECT is_employee FROM mst_customer WHERE id = $1;"
+
+	err = db.QueryRow(queryCheckEmployee, employeeId).Scan(&isEmployee)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to Check Employee Status"})
+		return
+	}
+
+	if !isEmployee {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Failed to Deleted This Customer, Forbidden!"})
+		return
+	}
+
+	query := "DELETE FROM mst_customer WHERE id = $1 RETURNING *;"
+
+	row, _ := db.Exec(query, employeeId)
+	if rowAffected, _ := row.RowsAffected(); rowAffected == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully Deleted Employee '" + id + "'", "data": "OK"})
 }
